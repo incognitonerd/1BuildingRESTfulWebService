c91c293638d9ff1df65e8234dbec507c5e4a82a2
Argos <Argos@172.25.201.200>
2017-05-26 11:28:26 -0400

    First Commit

---
+++ 1BuildingRESTfulWebService/src/main/java/hello/Greeting.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class Greeting {
+    private final long id;
+    private final String msg;
+
+    public Greeting(long id, String msg) {
+        this.id = id;
+        this.msg = msg;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return msg;
+    }
+}
