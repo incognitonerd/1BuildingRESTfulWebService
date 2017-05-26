2bb501a8799af325026b9fb99569313cadfd11b3
Argos <Argos@172.25.201.200>
2017-05-26 11:38:31 -0400

    4th Commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-/*
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-*/
+
+    //http://localhost:8080/greeting
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
